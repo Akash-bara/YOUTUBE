f8ee6a7d356623f900f9f3cc27d87e92a364a334
Akash-bara <akash80920@gmail.com>
2026-02-01 12:48:56 +0530

    modified  Environment Variables

--- src/utils/constant.jsx
+++ src/utils/constant.jsx
@@ -26,7 +26,7 @@ export const List =[
   
   export const YOUTUBE_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY;
 
-export const YOUTUBE_SEARCH_API="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+export const YOUTUBE_SEARCH_API=import.meta.env.VITE_SEARCH_API_KEY;
 
 export const YOUTUBE_COMMENT_PIC="https://www.nicepng.com/png/detail/119-1196111_comment-youtube-comment-icon-png.png"
 
